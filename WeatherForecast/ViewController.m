2d40991a00bc2024fcca6bc07b72556183dabec9
Alexey Naumov <a.naumov91@gmail.com>
2015-10-25 15:21:31 +0600

    Basic UI

--- WeatherForecast/ViewController.m
+++ WeatherForecast/ViewController.m
@@ -8,11 +8,51 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *temperatureLabel;
+@property (strong, nonatomic) NSArray *locations;
 
 @end
 
 @implementation ViewController
 
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self loadLocations];
+}
+
+- (void)loadLocations
+{
+  self.locations = @[
+                     @"Current location",
+                     @"London",
+                     @"Paris",
+                     @"Tokyo",
+                     @"New York",
+                     ];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+{
+  return [self.locations count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReuseID"];
+  cell.textLabel.text = self.locations[indexPath.row];
+  return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  
+}
 
 @end
